99ac85731f9cdac11c8a286267bdb1940fe4ffa1
Nicolas Rios <Nickrios33@yahoo.com>
2022-10-23 16:36:48 -0700

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Nicolas Rios on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
